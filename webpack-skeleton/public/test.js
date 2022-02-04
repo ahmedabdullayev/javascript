ccc616da4558c0a4e44836f3b6b2632507e17716
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2022-02-04 18:05:46 +0200

    hw1: js simple calculator

--- webpack-skeleton/public/test.js
+++
@@ -1,2 +0,0 @@
-let z = 5;
-console.log(z - 2, z + 2);
--- webpack-skeleton/src/index.js
+++ webpack-skeleton/src/index.js
@@ -1,1 +1,39 @@
-console.log("index jsese")
+
+// GET OUR ELEMENTS
+let first_number = document.querySelector('#first_number')
+let second_number = document.querySelector('#sec_number')
+let operation = document.querySelector('#operation')
+let button = document.querySelector('#calculateit')
+
+function getFirstNumber() {
+   return parseFloat(first_number.value);
+}
+
+function getSecondNumber() {
+    return parseFloat(second_number.value);
+}
+
+function getOperation() {
+    return operation.value;
+}
+
+button.onclick = function () {
+    let text = resultOf(getFirstNumber(), getSecondNumber(), getOperation())
+
+    if(getOperation() == "+"){
+        alert(text + getFirstNumber()+getSecondNumber()); //alerts result
+    }
+    if(getOperation() == "-"){
+        alert(text + getFirstNumber()-getSecondNumber());
+    }
+    if(getOperation() == "/"){
+        alert(text + getFirstNumber()/getSecondNumber());
+    }
+    if(getOperation() == "*"){
+        alert(text + getFirstNumber()*getSecondNumber());
+    }
+}
+
+function resultOf(num1, num2, operation) {
+    return "Result of: " + num1 +" " + operation + " " + num2 +" = "
+}
