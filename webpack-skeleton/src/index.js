5c2da86a7c6b21377030e7402306f68ff532cc6e
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2022-02-26 13:31:18 +0200

    hw3 fixes

--- webpack-skeleton/src/index.js
+++ webpack-skeleton/src/index.js
@@ -3,116 +3,74 @@ import MainView from "../public/Views/MainView";
 import NumberButtonsView from "../public/Views/NumberButtonsView";
 import CalcController from "../public/Controllers/CalcController";
 
-// let btnsView = NumberButtonsView();
-// let viewer = MainView(btnsView);
-//
-// let my_calc = new CalcBrain();
-// let my_controller = new CalcController(my_calc);
-// document.querySelector('.container').appendChild(viewer)
-//
-// document.querySelector('#quickmath').addEventListener("click", (e) => {
-//     my_controller.setValues(e, false, false)
-// })
-// document.querySelector('#dot').addEventListener("click", (e) => {
-//     my_controller.setValues(null, true, false)
-// })
-// document.querySelector('#toplusorminus').onclick= function (){
-//     my_controller.setValues(null, false, true)
-// }
-// document.querySelector('#clearnum').onclick= function (){
-//     my_controller.setValues(null, false, false, true)
-// }
-//
-// document.querySelector('#plusik').onclick = function () {
-//     my_controller.doCalculations('plus')
-// }
-// document.querySelector('#minusik').onclick = function () {
-//     my_controller.doCalculations('minus')
-// }
-// document.querySelector('#multiply').onclick = function () {
-//     my_controller.doCalculations('multiply')
-// }
-// document.querySelector('#divide').onclick = function () {
-//     my_controller.doCalculations('divide')
-// }
-// document.querySelector('#todefault').onclick = function () {
-//     my_controller.toDefaultValues()
-// }
-//
-// document.querySelector('#equals').onclick = function () {
-//     my_controller.doCalculations(my_calc.getLastOperation());
-// }
 
-
-
-
-function addNewGame(globalNum) {
+function addNewCalc(globalNum) {
 
 let order = "_0"+globalNum;
 
-let btnsView_01 = NumberButtonsView();
-let viewer_01 = MainView(btnsView_01, order);
+let btnsView = NumberButtonsView();
+let viewer = MainView(btnsView, order);
 
-let my_calc_01 = new CalcBrain();
-let my_controller_01 = new CalcController(my_calc_01, order);
-document.querySelector('.container').appendChild(viewer_01)
+let my_calc = new CalcBrain();
+let my_controller = new CalcController(my_calc, order);
+document.body.appendChild(viewer)
 
 document.querySelector('#quickmath'+order).addEventListener("click", (e) => {
-    my_controller_01.setValues(e, false, false)
+    my_controller.setValues(e, false, false)
 })
 document.querySelector('#dot'+order).addEventListener("click", (e) => {
-    my_controller_01.setValues(null, true, false)
+    my_controller.setValues(null, true, false)
 })
 document.querySelector('#toplusorminus'+order).onclick= function (){
-    my_controller_01.setValues(null, false, true)
+    my_controller.setValues(null, false, true)
 }
 document.querySelector('#clearnum'+order).onclick= function (){
-    my_controller_01.setValues(null, false, false, true)
+    my_controller.setValues(null, false, false, true)
 }
 
 document.querySelector('#plusik'+order).onclick = function () {
-    my_controller_01.doCalculations('plus')
+    my_controller.doCalculations('plus')
 }
 document.querySelector('#minusik'+order).onclick = function () {
-    my_controller_01.doCalculations('minus')
+    my_controller.doCalculations('minus')
 }
 document.querySelector('#multiply'+order).onclick = function () {
-    my_controller_01.doCalculations('multiply')
+    my_controller.doCalculations('multiply')
 }
 document.querySelector('#divide'+order).onclick = function () {
-    my_controller_01.doCalculations('divide')
+    my_controller.doCalculations('divide')
 }
 document.querySelector('#todefault'+order).onclick = function () {
-    my_controller_01.toDefaultValues()
+    my_controller.toDefaultValues()
 }
 
 document.querySelector('#equals'+order).onclick = function () {
-    my_controller_01.doCalculations(my_calc_01.getLastOperation());
+    my_controller.doCalculations(my_calc.getLastOperation());
 }
 
 }
 
-let globalNum = 0;
+let globalCounter = 0;
 
 let buttonAdd = document.createElement("button");
-buttonAdd.textContent = "Add Game";
-document.querySelector('.container').appendChild(buttonAdd)
+buttonAdd.textContent = "Add Calc";
+document.body.appendChild(buttonAdd)
 
 buttonAdd.onclick = function(){
-    globalNum++;
-    addNewGame(globalNum);
+    globalCounter++;
+    addNewCalc(globalCounter);
 }
 
 let buttonRemove = document.createElement("button");
-buttonRemove.textContent = "Remove Game";
-document.querySelector('.container').appendChild(buttonRemove)
+buttonRemove.textContent = "Remove Calc";
+document.body.appendChild(buttonRemove)
 
 buttonRemove.onclick = function(){
-    removeGame(globalNum);
-    globalNum--;
+    removeCalc(globalCounter);
+    globalCounter--;
 }
 
-function removeGame(calc) {
-   let s = document.querySelector('.container').querySelector(`#calc_0${calc}`).remove()
+function removeCalc(calc) {
+   document.body.querySelector(`#calc_0${calc}`).remove()
 }
 
