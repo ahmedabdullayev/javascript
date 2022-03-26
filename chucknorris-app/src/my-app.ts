b89a38d0e805830c225117b49c8e90bb39d87fb4
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2022-03-26 12:33:36 +0300

    hw2 done - better style

--- chucknorris-app/src/my-app.ts
+++ chucknorris-app/src/my-app.ts
@@ -1,37 +1,29 @@
-import { customElement, IRouter, inject, ILogger, route, IDisposable, ICustomElementViewModel } from 'aurelia';
+import { ILogger, route} from 'aurelia';
 import {JokeService} from "./services/jokeService";
 @route({
   routes: [
     {
       id: 'main',
       path: '',
-      transitionPlan: "invoke-lifecycles",
       component: import('./components/index'),
       title: 'Main',
     },
     {
       id: 'results',
       path: '/category/:cat',
-      transitionPlan: "invoke-lifecycles",
       component: import('./components/category'),
       title: 'Results',
     },
 
   ]
 })
-@inject()
-export class MyApp implements ICustomElementViewModel{
-  public message = 'Hello World!';
-  private _subscriptions: IDisposable[] = [];
+export class MyApp{
   private categories : string[] = [];
   constructor(
-      @IRouter private router: IRouter,
       @ILogger private logger: ILogger,
       private JokeService: JokeService
   ) {
     this.logger = logger.scopeTo("MyApp");
-  }
-  async load(){
   }
   async created(){
      let data = await this.JokeService.getRandomCategories(3);
